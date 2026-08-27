c1456227e8dd5b784c050e3064702fb1814fbf95
Chandan Usman Gani <chandanusmangani@gmail.com>
2026-08-27 20:58:43 +0530

    feat: Sensor data fecth endpoint

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+import json
 
 app = FastAPI()
 
@@ -13,7 +14,16 @@ def health():
 async def collect_sensor_data(req: Request):
     try:
         body = await req.json()
+        #save the data in a file 
+        with open("sensor_data.json", "a") as f:
+            json.dump(body, f)
+
         print("Data received: ", body)
         return {"message": "Sensor data received successfully", "Data : " : body}
     except Exception as e:
         return {"status": "error", "message": "Failed to parse sensor data"}  
+
+@app.get("/api/sensor")
+def get_sensor_data():
+    with open("sensor_data.json", "r") as f:
+        return json.load(f)
